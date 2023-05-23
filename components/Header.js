c1fdb9a0d5ea78bffc94836e420e7cc759ec2199
Roshan <roshankumar40786@gmail.com>
2023-05-23 15:49:13 +0530

    making responsive header components

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,12 @@
-import React from 'react'
-import { BsTwitter } from 'react-icons/bs'
+import React, { useState } from 'react';
+import { BsTwitter } from 'react-icons/bs';
+
 const Header = () => {
+  const [user] = useState([
+    { userPersonal: "For you" },
+    { userPersonal: "Following" },
+  ]);
+
   return (
     <div className='sticky top-0 bg-white px-[16px] flex flex-col'>
       <div className='md:hidden flex items-center justify-between'>
@@ -9,12 +15,24 @@ const Header = () => {
         <div></div>
       </div>
       <h2 className='font-bold text-black'>Home</h2>
-      <ul className='flex justify-between text-center'>
-        <li className='flex-1 font-bold'>For you</li>
-        <li className='flex-1 font-bold text-[#536471]'>Following</li>
-      </ul>
+      <div className='flex justify-between '>
+        {user.map((users, index) => (
+          <div className=' hover:bg-slate-100 flex-1  justify-center flex'>
+            <p
+              key={index}
+              className={`font-bold cursor-pointer py-4 ${index === 0 ? 'relative' : ''
+                }`}
+            >
+              {index === 0 && (
+                <span className="absolute bottom-0 left-0 w-[100%] h-[4px] bg-[#1D9BF0]"></span>
+              )}
+              {users.userPersonal}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
