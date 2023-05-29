53ef3243500d9c7f467967489963879a97306674
roshan <roshankumar40786@gmail.com>
2023-05-29 12:59:22 +0530

    change icon size in mobileMenu

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -16,10 +16,10 @@ const MobileMenu = () => {
     <div className=' md:hidden  fixed bottom-0 border right-0 left-0  bg-white list-none'>
       <ul className='p-[8px] flex justify-around'>
         {MobileMenuIcon.map((icon, index) => (
-          <li className='cursor-pointer p-[8px]' key={index}>
+          <li className='cursor-pointer' key={index}>
             <Link href={`${icon.url}`}>
               {React.cloneElement(icon.Icon, {
-                style: { width: '42px', height: '42px' },
+                style: { width: '42px', height: '42px' ,padding:"8px"},
               })}
             </Link>
           </li>
